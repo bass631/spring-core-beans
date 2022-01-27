a79f7081afa457a4244118e431a75f3ea7e19d52
bass631 <dima.bass@mail.ru>
2022-01-27 23:32:08 +0300

    2.1.3 v2

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -23,7 +23,6 @@ public class AnimalsCage {
         System.out.println(animal.toString());
         System.out.println("At:");
         System.out.println(new Timer().getTime());
-        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 }
